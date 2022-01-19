401984220e6cc27fbb5e7a451ead2cef8b67b3ae
tom-scheyltjens <tom_scheyltjens@outlook.com>
2022-01-19 12:02:43 +0100

    feat(story 1): added validation to CreateMemberDTO, added custom PasswordValidator

--- src/main/java/com/example/timrocket_backend/api/MemberController.java
+++ src/main/java/com/example/timrocket_backend/api/MemberController.java
@@ -5,10 +5,15 @@ import com.example.timrocket_backend.service.dto.CreateMemberDTO;
 import com.example.timrocket_backend.service.dto.MemberDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -26,10 +31,25 @@ public class MemberController {
     }
 
     @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
-    public MemberDTO createMember(@RequestBody CreateMemberDTO createMemberDTO){
+    @ResponseStatus(HttpStatus.CREATED)
+    public MemberDTO createMember(@Valid @RequestBody CreateMemberDTO createMemberDTO){
 
         MemberDTO memberDTO = memberService.createMember(createMemberDTO);
 
         return memberDTO;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+            logger.error(errorMessage, exception);
+        });
+        return errors;
     }
 }
---
+++ src/main/java/com/example/timrocket_backend/service/dto/PasswordValidator.java
@@ -0,0 +1,18 @@
+package com.example.timrocket_backend.service.dto;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.lang.annotation.Annotation;
+
+public class PasswordValidator implements ConstraintValidator<PasswordConstraint, String> {
+    @Override
+    public void initialize(PasswordConstraint password) {
+    }
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext cxt) {
+        return password !=null &&
+                password.trim().length() !=0 &&
+                password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$");
+    }
+}
