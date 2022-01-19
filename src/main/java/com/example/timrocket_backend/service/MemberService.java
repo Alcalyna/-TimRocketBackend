2d34a91684b271a45bab0019890118d0ca4904f0
tom-scheyltjens <tom_scheyltjens@outlook.com>
2022-01-19 14:32:19 +0100

    feat(story 1): member gets added to keycloack after saving in database

--- src/main/java/com/example/timrocket_backend/service/MemberService.java
+++ src/main/java/com/example/timrocket_backend/service/MemberService.java
@@ -2,6 +2,9 @@ package com.example.timrocket_backend.service;
 
 import com.example.timrocket_backend.domain.Member;
 import com.example.timrocket_backend.repository.MemberRepository;
+import com.example.timrocket_backend.security.Role;
+import com.example.timrocket_backend.security.SecurityMemberDTO;
+import com.example.timrocket_backend.security.SecurityService;
 import com.example.timrocket_backend.service.dto.CreateMemberDTO;
 import com.example.timrocket_backend.service.dto.MemberDTO;
 import com.example.timrocket_backend.service.mapper.MemberMapper;
@@ -12,18 +15,18 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final MemberMapper memberMapper;
+    private final SecurityService securityService;
 
-    public MemberService(MemberRepository memberRepository, MemberMapper memberMapper) {
+    public MemberService(MemberRepository memberRepository, MemberMapper memberMapper, SecurityService securityService) {
         this.memberRepository = memberRepository;
         this.memberMapper = memberMapper;
+        this.securityService = securityService;
     }
 
     public MemberDTO createMember(CreateMemberDTO createMemberDTO) {
         Member member = memberMapper.createMemberDtoToMember(createMemberDTO);
-
-
-
         memberRepository.save(member);
+        securityService.addMember(new SecurityMemberDTO(member.getEmail(), member.getPassword(), Role.valueOf(member.getRole().name())));
         MemberDTO memberDTO = memberMapper.memberToMemberDto(member);
         return memberDTO;
     }
