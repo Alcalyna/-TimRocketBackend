f1528e257a690076f882be40a9fb5abdaa8b2b17
Linh P. Nguyen <nguyenlp.info@gmail.com>
2022-02-02 10:38:01 +0100

    feat(Story 5): get a user by id

--- src/main/java/com/example/timrocket_backend/api/UserController.java
+++ src/main/java/com/example/timrocket_backend/api/UserController.java
@@ -75,15 +75,22 @@ public class UserController {
     @GetMapping(produces = APPLICATION_JSON_VALUE, path = "/{id}")
     @ResponseStatus(HttpStatus.OK)
     public UserDTO getUserById(@PathVariable UUID id){
-        return userService.getById(id);
+        logger.info("Get User By Id started");
+        UserDTO userDTO = userService.getById(id);
+        logger.info("Get User By Id finished");
+        return userDTO;
     }
 
     @PreAuthorize("hasAuthority('GET_COACH_INFORMATION')")
     @GetMapping(produces = APPLICATION_JSON_VALUE, params = {"coach"}, path = "/{id}")
     @ResponseStatus(HttpStatus.OK)
     public CoachDTO getCoachById(@PathVariable UUID id, @RequestParam String coach){
+        System.out.println("I am here ");
+        System.out.println("The id is " + id);
         return userService.getCoachBy(id);
     }
+
+//    public CoachDTO getCoachProfile(@PathVariable UUID id)
 
     @PutMapping(produces = APPLICATION_JSON_VALUE, path = "/{id}")
     @ResponseStatus(HttpStatus.OK)
